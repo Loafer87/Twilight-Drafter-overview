a4a6222bad1167f1b50d713b457997c9f753a914
Loafer87 <joshua.julien@gmail.com>
2026-09-02 21:58:14 -0700

    Use Cloudinary faction portraits for ceremonial reveals

--- te-art.js
+++ te-art.js
@@ -1,39 +1,59 @@
-const TE_PORTRAITS={
-  'Last Bastion':{
-    url:'https://images-cdn.fantasyflightgames.com/filer_public/f9/b6/f9b6d799-cdc5-48dc-af93-47cd68ebd022/ti11_faction-article_factions_sheet_lastbastion.png',
-    size:'260% auto',position:'71% 28%'
-  },
-  'The Deepwrought Scholarate':{
-    url:'https://images-cdn.fantasyflightgames.com/filer_public/10/20/102016e2-4737-4c57-97c3-7dee24f39f09/ti11_faction-article_factions_sheet_thedeepwroughtscholarate.png',
-    size:'260% auto',position:'80% 28%'
-  },
-  'The Crimson Rebellion':{
-    url:'https://images-cdn.fantasyflightgames.com/filer_public/aa/f4/aaf4c0bc-cc93-4b9c-bde3-dc5b1b4eb7ae/ti11_faction-article_factions_sheet_thecrimsonrebellion.png',
-    size:'260% auto',position:'71% 33%'
-  },
-  'The Ral Nel Consortium':{
-    url:'https://images-cdn.fantasyflightgames.com/filer_public/f0/4e/f04e4aa0-6b97-48e6-957c-233dcf2c9ccd/ti11_article_factions_sheet_theralnelconsortium.png',
-    size:'260% auto',position:'79% 35%'
-  },
-  'The Firmament':{
-    url:'https://images-cdn.fantasyflightgames.com/filer_public/c3/1d/c31d42da-dfcd-4889-87f6-6edd85a7e865/ti11_article_factions_sheet_thefirmament.png',
-    size:'260% auto',position:'72% 25%'
-  }
+const FACTION_PORTRAITS={
+  'The Arborec':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411044/ARBOREC_zntkho.webp',position:'50% 22%'},
+  'The Barony of Letnev':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411044/BARONY_nf7ish.webp',position:'50% 20%'},
+  'The Clan of Saar':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411047/SAAR_zuodve.webp',position:'50% 22%'},
+  'The Embers of Muaat':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411045/MUAAT_nwnmic.webp',position:'50% 22%'},
+  'The Emirates of Hacan':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411045/HACAN_qif8wy.webp',position:'50% 18%'},
+  'The Federation of Sol':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411048/SOL_lle7gy.webp',position:'50% 20%'},
+  'The Ghosts of Creuss':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411045/GHOSTS_yenpud.webp',position:'50% 19%'},
+  'The L1Z1X Mindnet':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411045/L1Z1XArt_qixkvw.webp',position:'50% 18%'},
+  'The Mentak Coalition':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411045/MENTAK_cl6znm.webp',position:'50% 22%'},
+  'The Naalu Collective':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411046/NAALU_ukqphr.webp',position:'50% 18%'},
+  'The Nekro Virus':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411047/NEKRO_mrghnl.webp',position:'50% 18%'},
+  "Sardakk N'orr":{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411048/SARDAKK_pvddse.webp',position:'50% 20%'},
+  'The Universities of Jol-Nar':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411045/JOLNAR_cncnfz.webp',position:'50% 20%'},
+  'The Winnu':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411049/WINNU_kh6itt.webp',position:'50% 20%'},
+  'The Xxcha Kingdom':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411049/XXCHA_e3arss.webp',position:'50% 20%'},
+  'The Yin Brotherhood':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411049/YIN_oei1n3.webp',position:'50% 20%'},
+  'The Yssaril Tribes':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411049/YSSARIL_jzdnhx.webp',position:'50% 20%'},
+  'The Argent Flight':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411044/ArgentAgent_P_kzlimr.webp',position:'50% 20%'},
+  'The Empyrean':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411044/EMPYREAN_gh3ja2.webp',position:'50% 20%'},
+  'The Mahact Gene-Sorcerers':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411045/MAHACT_oth6zf.webp',position:'50% 18%'},
+  'The Naaz-Rokha Alliance':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411047/NAAZROKHA_blad9f.webp',position:'50% 22%'},
+  'The Nomad':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411047/NOMAD_b7tp4f.webp',position:'50% 20%'},
+  'The Titans of Ul':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411048/TITANS_1_rnote0.webp',position:'50% 20%'},
+  "The Vuil'raith Cabal":{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411044/CabalAgentP_qfp0uf.webp',position:'50% 20%'},
+  'Last Bastion':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411044/Bastion_zmzy8y.webp',position:'50% 20%'},
+  'The Deepwrought Scholarate':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411044/Deepwrought_assmls.webp',position:'50% 20%'},
+  'The Crimson Rebellion':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411044/Crimson_jdlooj.webp',position:'50% 20%'},
+  'The Ral Nel Consortium':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411047/Ralnel_qluhdn.webp',position:'50% 20%'},
+  'The Firmament':{url:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411044/Firmament_yy5hj4.webp',position:'50% 20%',alternateUrl:'https://res.cloudinary.com/grwiz5ap/image/upload/v1788411047/Obsidian_qbr70b.webp'}
 };
 
-Object.entries(TE_PORTRAITS).forEach(([name,portrait])=>{
+const TE_REFERENCES={
+  'Last Bastion':'https://images-cdn.fantasyflightgames.com/filer_public/f9/b6/f9b6d799-cdc5-48dc-af93-47cd68ebd022/ti11_faction-article_factions_sheet_lastbastion.png',
+  'The Deepwrought Scholarate':'https://images-cdn.fantasyflightgames.com/filer_public/10/20/102016e2-4737-4c57-97c3-7dee24f39f09/ti11_faction-article_factions_sheet_thedeepwroughtscholarate.png',
+  'The Crimson Rebellion':'https://images-cdn.fantasyflightgames.com/filer_public/aa/f4/aaf4c0bc-cc93-4b9c-bde3-dc5b1b4eb7ae/ti11_faction-article_factions_sheet_thecrimsonrebellion.png',
+  'The Ral Nel Consortium':'https://images-cdn.fantasyflightgames.com/filer_public/f0/4e/f04e4aa0-6b97-48e6-957c-233dcf2c9ccd/ti11_article_factions_sheet_theralnelconsortium.png',
+  'The Firmament':'https://images-cdn.fantasyflightgames.com/filer_public/c3/1d/c31d42da-dfcd-4889-87f6-6edd85a7e865/ti11_article_factions_sheet_thefirmament.png'
+};
+
+Object.entries(FACTION_PORTRAITS).forEach(([name,portrait])=>{
   const faction=factions.find(f=>f.name===name);
-  if(faction){faction.portrait=portrait;faction.reference=portrait.url}
+  if(faction){
+    faction.portrait={...portrait,size:'cover'};
+    if(TE_REFERENCES[name])faction.reference=TE_REFERENCES[name];
+  }
 });
 
 function factionPortraitStyle(f){
   const p=f&&f.portrait;if(!p)return'';
-  return `background-image:url(${p.url});background-size:${p.size||'260% auto'};background-position:${p.position||'72% 30%'};background-repeat:no-repeat`;
+  return `background-image:url(${p.url});background-size:${p.size||'cover'};background-position:${p.position||'50% 20%'};background-repeat:no-repeat`;
 }
 
 const baseArtMarkupTE=artMarkup;
 artMarkup=function(f,cls=''){
-  if(f.portrait)return `<div class="card-art portrait-art ${cls}" style="${factionPortraitStyle(f)}" role="img" aria-label="${escapeAttr(f.name)} official faction artwork"></div>`;
+  if(f.portrait)return `<div class="card-art portrait-art ${cls}" style="${factionPortraitStyle(f)}" role="img" aria-label="${escapeAttr(f.name)} faction artwork"></div>`;
   return baseArtMarkupTE(f,cls);
 };
 
